25b7357b823423c8ad129e2904629899d5daea14
dtechdevelopers07 <55192607+dtechdevelopers07@users.noreply.github.com>
2020-07-01 17:20:04 +0500

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,17 @@
+# Hosting url to be used for adding server entry to database and show progress on web
+HOSTING_DOMAIN="http://http://frontline.agronexus.org/";
+#url for progress
+PROGRESS_URL=$HOSTING_DOMAIN"progress/";
+#url fot adding record to database;
+RECORD_URL=$HOSTING_DOMAIN"entry";
+# Some API Authentication Configuration
+API_URL="frontline.agronexus.org";
+API_SSL="1";
+API_PAGE="connection";
+API_USER_PARAM="username";
+API_PASS_PARAM="password";
+API_SERVER_PARAM="ip";
+API_PROCESS_PARAM="proc";
+API_IP=$1
+API_CONNECT_SCRIPT_PATH="/etc/ocserv/.../...";
+API_DISCONNECT_SCRIPT_PATH="/etc/ocserv/.../....";
