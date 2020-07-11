44ed76003d713b4097ddccbb8710cd6c9554fca4
dtechdevelopers07 <55192607+dtechdevelopers07@users.noreply.github.com>
2020-07-11 23:47:03 +0500

    Update cONN3t.sh

--- cONN3t.sh
+++ cONN3t.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 echo $SH_DATA;
- response=$(curl -k -d "$SH_DATA&type=0" -X POST $API_LINK); 
+response=$(curl -k -d "$SH_DATA&type=0" -X POST $API_LINK); 
 echo $response; 
-if [[ "$response" != "0" ]]
+done=$(echo $response | jq ."done" | sed -e 's/^"//' -e 's/"$//');
+found=$(echo $response | jq ."found" | sed -e 's/^"//' -e 's/"$//');
+server=$(echo $response | jq ."server" | sed -e 's/^"//' -e 's/"$//');
+proc=$(echo $response | jq ."proc" | sed -e 's/^"//' -e 's/"$//');
+if [[ "$found" != "0" ]]
+then
+        curl -k -d "user=$proc" "http://"$server"/disconnect.php";
+fi
+
+if [[ "$done" != "0" ]]
 then
         exit 1;
 fi
