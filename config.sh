bcd0ebb5a241b808a0a66982d3b975b9f0bef079
Tahir Mahmood <30719791+tahirmahm123@users.noreply.github.com>
2021-04-13 01:51:43 +0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,15 +1,14 @@
 # Hosting url to be used for adding server entry to database and show progress on web
-_DOMAIN="itelvpn.net"
-HOSTING_DOMAIN="https://"$_DOMAIN"/serversetup/";
+_DOMAIN="100456.xyz"
 #url for progress
 PROGRESS_URL=$HOSTING_DOMAIN"changeProgress.php?progress=";
 # Some API Authentication Configuration
 API_URL=\"$_DOMAIN\";
 API_SSL="1";
-API_PAGE=\"vpn.php\";
-API_USER_PARAM="u78234as";
-API_PASS_PARAM="p23jsdf";
-API_SERVER_PARAM="serverhebhai";
+API_PAGE=\"login_from_server\";
+API_USER_PARAM="username";
+API_PASS_PARAM="password";
+API_SERVER_PARAM="server";
 API_PROCESS_PARAM="process";
 API_CONNECT_SCRIPT_PATH="/etc/ocserv/.../...";
 API_DISCONNECT_SCRIPT_PATH="/etc/ocserv/.../....";
